dce41fcfc164906db6bc11f10e80ce86ddc155fa
Matthew Schulkind <mschulkind@gmail.com>
2014-02-04 23:25:47 -0500

    Clean up the purge script and remove redundant zeroings.

--- common/purge.sh
+++ common/purge.sh
@@ -39,15 +39,8 @@ echo "Cleaning free space..."
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
-# whiteout root
-echo "Cleaning up /..."
-count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`; 
-dd if=/dev/zero of=/tmp/whitespace bs=1024;
-rm /tmp/whitespace;
-  
 # whiteout /boot
 echo "Cleaning up /boot..."
-count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 dd if=/dev/zero of=/boot/whitespace bs=1024;
 rm /boot/whitespace;
 
